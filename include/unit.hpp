316291e888044751067a8160346ed7259574df6c
Tsuki Superior <tsukisuperior@tutanota.com>
2021-05-07 07:42:00 -0500

    getting there

--- include/unit.hpp
+++ include/unit.hpp
@@ -7,8 +7,25 @@ class Unit
   public:
     uint16_t x;
     uint16_t y;
+
+    // Stats
     uint16_t attack;
     uint16_t defense;
+    uint16_t speed;
+    uint16_t vitality;
+    uint16_t power;
+    uint16_t resistance;
+    uint16_t agility;
+    uint16_t stamina;
+
+    // Rechargeable Stats
+    uint32_t health;
+    uint32_t energy;
+
+    // Level
+    uint16_t level;
 
     uint16_t calculateDamageDealt(const Unit &enemy);
+    uint32_t getmaxhealth(void);
+    uint32_t getmaxenergy(void);
 };
--- src/unit.cpp
+++ src/unit.cpp
@@ -4,3 +4,12 @@ uint16_t Unit::calculateDamageDealt(const Unit &enemy)
 {
     return static_cast<uint16_t>(round(attack / enemy.defense));
 }
+
+uint32_t Unit::getmaxhealth(void)
+{
+    return vitality * 10;
+}
+uint32_t Unit::getmaxenergy(void)
+{
+    return stamina * 5;
+}
